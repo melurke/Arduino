510513b63a66b06cc9da2fd04fa773eb4303afe0
melurke <101022215+melurke@users.noreply.github.com>
2022-05-24 19:31:09 +0200

    Create while_loop_button.c

---
+++ while_loop_button.c
@@ -0,0 +1,11 @@
+void setup() {
+  pinMode(4, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+  while(digitalRead(4)==1) {}
+  Serial.print("von jetzt");
+  while(digitalRead(4)==0) {}
+  Serial.print(" bis jetzt");
+}
