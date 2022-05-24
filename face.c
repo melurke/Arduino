b7149ce857bd9d810e39a8c36fc1aa0fc705f4b5
melurke <101022215+melurke@users.noreply.github.com>
2022-05-24 19:26:22 +0200

    Create face.c

---
+++ face.c
@@ -0,0 +1,12 @@
+void setup() {
+  Serial.begin(9600);
+  Serial.println(" sss ");
+  Serial.println("-~~~-");
+  Serial.println("x   x");
+  Serial.println("  o  ");
+  Serial.println("|___|");
+}
+
+void loop() {
+  
+}
